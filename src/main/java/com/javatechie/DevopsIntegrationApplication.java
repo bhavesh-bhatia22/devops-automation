68b6bb207bd3d0fc2d3aee08cf25b471725f3045
bhavesh-bhatia22 <bhaveshmb195@gmail.com>
2023-04-20 10:06:21 +0530

    first commit

---
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -0,0 +1,8 @@
+package com.javatechie;
+
+public class DevopsIntegrationApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This project is created by Roll no 21 and 7");
+    }
+}
